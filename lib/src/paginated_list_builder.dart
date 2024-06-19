3d46a87f537700cfb02bdfb52cf18e322947d7a9
Jonah <jonahgrosshanten@aol.com>
2024-06-19 11:42:19 +0200

    chore: Update Flutter Riverpod package import

--- lib/src/paginated_list_builder.dart
+++ lib/src/paginated_list_builder.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod/riverpod.dart';
 import 'package:riverpod_infinite_scroll_pagination/riverpod_infinite_scroll_pagination.dart';
 
 class PaginatedListBuilder<T> extends StatefulWidget {
